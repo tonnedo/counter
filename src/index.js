b49bf84abafb28cf0fe00f106b47e36077c0f239
Aidar Safiullin <tonnedo@gmail.com>
2019-10-01 01:13:03 +0300

    add react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () =>{
+  return (
+    <div>
+      <div>0</div>
+      <button>Click me</button>
+    </div>
+  )
+};
+ReactDOM.render(<App />, document.getElementById("app"));
