acc1f6de714ee141e10367fc9f21bd00ba4fcb3c
Aidar Safiullin <tonnedo@gmail.com>
2019-10-02 00:29:14 +0300

    counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+class Button extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: this.props.start,
+			countMax: this.props.max,
+			countMin: this.props.min
+		};
+	}
+	plus() {
+		this.setState({
+			// if(this.state.count <= this.props.max) {
+				count: this.state.count + 1
+			// }
+		});
+	}
+	minus() {
+		this.setState({
+			count: this.state.count - 1
+		});
+	}
+	render() {
+		const flex = {
+			display: 'flex',
+			width: 200,
+			margin: 'auto',
+			justifyContent: 'space-around'
+		}
+		const count = {
+			fontSize: 40
+		}
+		const btn = {
+			alignSelf: 'center'
+		}
 
-const App = () =>{
-  return (
-    <div>
-      <div>0</div>
-      <button>Click me</button>
-    </div>
-  )
-};
-ReactDOM.render(<App />, document.getElementById("app"));
+		return (
+			<div style={flex}>
+				<button style={btn} onClick={this.minus.bind(this)}>-</button>
+				<div style={count}>{this.state.count}</div>
+				<button style={btn} onClick={this.plus.bind(this)}>+</button>
+			</div>
+			);
+	}
+}
+render(<Button start={0} countMin={-10} countMax={10}/>, document.getElementById('btn'));
