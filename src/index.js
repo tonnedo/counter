bc45b389365ca10ac27001486dc41ea6e8ab2d52
Aidar Safiullin <tonnedo@gmail.com>
2019-10-03 20:40:15 +0300

    change counter

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-class Button extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			count: this.props.start,
-			countMax: this.props.max,
-			countMin: this.props.min
-		};
-	}
-	plus() {
-		this.setState({
-			// if(this.state.count <= this.props.max) {
-				count: this.state.count + 1
-			// }
-		});
-	}
-	minus() {
-		this.setState({
-			count: this.state.count - 1
-		});
-	}
-	render() {
-		const flex = {
-			display: 'flex',
-			width: 200,
-			margin: 'auto',
-			justifyContent: 'space-around'
-		}
-		const count = {
-			fontSize: 40
-		}
-		const btn = {
-			alignSelf: 'center'
-		}
+import './counter.css';
 
-		return (
-			<div style={flex}>
-				<button style={btn} onClick={this.minus.bind(this)}>-</button>
-				<div style={count}>{this.state.count}</div>
-				<button style={btn} onClick={this.plus.bind(this)}>+</button>
-			</div>
-			);
-	}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: this.props.start
+    }
+
+    this.handleClickPlus = this.handleClickPlus.bind(this)
+    this.handleClickMinus = this.handleClickMinus.bind(this)
+  }
+
+  handleClickPlus () {
+    if(this.state.count >= this.props.max) {
+      return
+    }
+
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  handleClickMinus () {
+    if (this.state.count <= this.props.min) {
+      return;
+    }
+
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+  render() {
+    const { count } = this.state
+
+    return (
+      <div className="flex">
+        <button className="btn" onClick={this.handleClickMinus}>-</button>
+          <div className="counter">{count}</div>
+        <button className="btn" onClick={this.handleClickPlus}>+</button>
+      </div>
+      )
+  }
 }
-render(<Button start={0} countMin={-10} countMax={10}/>, document.getElementById('btn'));
+
+render(<Counter start={0} min={-10} max={10} />, document.getElementById('btn'))
